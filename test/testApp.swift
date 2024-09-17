970ebc07d11ee09b3e101f545f0bb31f4261ff43
imo2k <dnrwk99@gmail.com>
2024-09-17 11:28:15 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by GO on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
